77aac0d8c80b00c5b27f4a68b66cfa9a59295480
MiaoJingyi <miaojingyi86@gmail.com>
2023-11-06 22:58:45 +0800

    .

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
-import "fmt"
-
-const (
-	i = 7 + iota
-	j
-	k
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
 )
 
+type A struct {
+	Id   string
+	Name string
+}
+
+type B struct {
+	Id   string
+	Name string
+}
+
 func main() {
-	fmt.Println(i, j, k)
+	a := "aaabbbccc"
+	ssh := *(*reflect.StringHeader)(unsafe.Pointer(&a))
+	fmt.Printf("%#v\n", ssh)
+	b := *(*[]byte)(unsafe.Pointer(&ssh))
+	fmt.Printf("%s\n", b)
+
+	s1 := A{Id: "1", Name: "A"}
+	s2 := *(*B)(unsafe.Pointer(&s1))
+	fmt.Printf("%#v", s2)
 }
